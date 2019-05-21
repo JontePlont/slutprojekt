aa5943b12d8f2c82772b445daaa3d93b30c8d6ad
JontePlont <44798527+JontePlont@users.noreply.github.com>
2019-05-21 09:59:21 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
-window.onload = function(){// Set the date we're counting down to
-var countDownDate = new Date("May 29, 2019 00:00:00").getTime();
+//FUnktion för nedräkning till tjejhack
+window.onload = function(){// Här skrivs datum för nästa event in
+var datum = new Date("May 27, 2019 08:30:00").getTime();
 
-// Update the count down every 1 second
+// Countdown ska uppdateras varje sekund
 var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+  // Hämtar dagens datum
+  var nu = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  // Räknar ut hur lång tid det är från dagens datum till datumet då eventet sker
+  var tidkvar = datum - nu;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Räknar ut dagar, timmar, minuter och sekunder
+  var dag = Math.floor(tidkvar / (1000 * 60 * 60 * 24));
+  var timmar = Math.floor((tidkvar % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minut = Math.floor((tidkvar % (1000 * 60 * 60)) / (1000 * 60));
+  var sekund = Math.floor((tidkvar % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + " dagar " + hours + " timmar "
-  + minutes + " minuter " + seconds + " sekunder ";
+  // Skriver ut det i div med id "demo"
+  document.getElementById("demo").innerHTML = dag + " dagar " + timmar + " timmar "
+  + minut + " minuter " + sekund + " sekunder ";
 
-  // If the count down is finished, write some text 
-  if (distance < 0) {
+  // Skriver text om datumet passerat
+  if (tidkvar < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "Information om nästa tjejhack kommer snart!";
   }
 }, 1000);
 }
